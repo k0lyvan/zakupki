ce0ac189cce0e5a0edcb4e8f8aec7ce61855a8d3
MaximKol <mk.kolybanov@mail.ru>
2026-02-24 20:06:16 +0300

    создан класс закупки

---
+++ zakupki/Zakupka.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zakupki
+{
+    internal struct Price
+    {
+        public decimal prices { get; set; }
+        public enum valut { RUB, EUR, USD }
+    }
+    internal struct Pozition
+    {
+        public string name { get; set; }
+        public int count { get; set; }
+        public string unit { get; set; }
+        public Price pricePozition { get; set; }
+    }
+    internal class Zakupka
+    {
+        public string url {  get; set; }
+        public int id { get; set; }
+        public string discripton { get; set; }
+        public DateTimeOffset dateStart { get; set; }
+        public DateTimeOffset dateEnd { get; set; }
+        public Price purchasePrice { get; set; }
+        public string FioContact { get; set; }
+        public string contactNumber { get; set; }
+        public string companyName { get; set; }
+
+        public Pozition[] purchasePozition { get; set; }
+    }
+}
