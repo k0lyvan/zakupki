abf1e4e9be01c801889c507aa022ebba960ca3b6
MaximKol <mk.kolybanov@mail.ru>
2026-02-24 18:24:48 +0300

    Добавьте файлы проекта.

---
+++ zakupki/Program.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Playwright;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace zakupki
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            
+
+            using var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var url = "https://apizakupki.nefteavtomatika.ru/api/purchases?page=1&order_by[number]=desc&per_page=30";
+
+            await using var stream = await client.GetStreamAsync(url);
+
+            var doc = await JsonDocument.ParseAsync(stream);
+
+            Console.WriteLine(
+                JsonSerializer.Serialize(doc, new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                })
+            );
+        }
+    } 
+}
